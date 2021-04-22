f282fd54277d449d37b633d6ad840be45e2b2a77
Vladislav Zaporozhets <piman131@yandex.ua>
2021-04-22 20:40:34 +0300

    fix main_images text and add grid layout to image gallery

--- src/components/countrieBlockComponents/countrieBlockGallery.js
+++ src/components/countrieBlockComponents/countrieBlockGallery.js
@@ -3,49 +3,58 @@ import classes from '../../styles/countrieBlockStyles/countrieBlockGallery.modul
 
 
 import niderlandsImg from '../../img/countries/nider.png'
-import maldives from '../../img/countries/mal.png'
-import vengria from '../../img/countries/vengr.png'
-import mramCanyon from '../../img/countries/mram.png'
-import newYork from '../../img/countries/NY.png'
-import kanada from '../../img/countries/kanada.png'
+import maldivesImg from '../../img/countries/mal.png'
+import vengriaImg from '../../img/countries/vengr.png'
+import mramCanyonImg from '../../img/countries/mram.png'
+import newYorkImg from '../../img/countries/NY.png'
+import kanadaImg from '../../img/countries/kanada.png'
 import { KANADA_TEXT, MALDIVES_TEXT, MRAM_KANYON_TEXT, NIRERLAND_TEXT, NY_TEXT, VENGRIA_TEXT } from './countrieBlockText'
 
 export default function CountrieBlockGallery() {
 	return (
-		<div>
-			<MainContentImg
-				imgSrc={niderlandsImg}
-				title="Нидерланды"
-				content__text_subtitle={NIRERLAND_TEXT}
-			/>
-			<div className={classes.flexWrapper}>
+		<div className={classes.imgGallary}>
+			<div className={classes.niderlands}>
 				<MainContentImg
-					imgSrc={maldives}
+					imgSrc={niderlandsImg}
+					title="Нидерланды"
+					content__text_subtitle={NIRERLAND_TEXT}
+				/>
+			</div>
+			<div className={classes.maldives}>
+				<MainContentImg
+					imgSrc={maldivesImg}
 					title="Мальдивы"
 					content__text_subtitle={MALDIVES_TEXT}
 				/>
+			</div>
+			<div className={classes.vengria}>
 				<MainContentImg
-					imgSrc={vengria}
+					imgSrc={vengriaImg}
 					title="Венгрия"
 					content__text_subtitle={VENGRIA_TEXT}
 				/>
 			</div>
-			<MainContentImg
-				imgSrc={mramCanyon}
-				title="Мраморный каньон"
-				content__text_subtitle={MRAM_KANYON_TEXT}
-			/>
-			<div className={classes.flexWrapper}>
+			<div className={classes.mram}>
 				<MainContentImg
-					imgSrc={newYork}
+					imgSrc={mramCanyonImg}
+					title="Мраморный каньон"
+					content__text_subtitle={MRAM_KANYON_TEXT}
+				/>
+			</div>
+			<div className={classes.newYork}>
+				<MainContentImg
+					imgSrc={newYorkImg}
 					title="Нью Йорк"
 					content__text_subtitle={NY_TEXT}
 				/>
+			</div>
+			<div className={classes.kanada}>
 				<MainContentImg
-					imgSrc={kanada}
+					imgSrc={kanadaImg}
 					title="Канада"
 					content__text_subtitle={KANADA_TEXT}
 				/>
 			</div>
-		</div>)
+		</div>
+	)
 }
