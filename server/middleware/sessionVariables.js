da1bbd0391169f937cd5eeea70a32f46d4442a01
Vladislav Zaporozhets <vladislav.zaporozhets@haiku.dev>
2021-11-09 23:38:55 +0300

    .

--- server/middleware/sessionVariables.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (res, req, next) {
-	// res.locals.isAuth = req.session.isAuthenticated
-	console.log(req.session)
-	next()
-}
--- src/components/advantagesSquares/advantagesText.js
+++
@@ -1,8 +0,0 @@
-export const FIRST_ITEM_TEXT =
-	'Мы предоставляем широкий \n выбор стран и городов'
-export const SECOND_ITEM_TEXT =
-	`С нами вы будете уверенны в \n том, что прилетите куда надо`
-export const THIRD_ITEM_TEXT =
-	`Поездка, отель, экскурсии и \n никаких переплат`
-export const FOURTH_ITEM_TEXT =
-	`Наша фирма находится на рынке \n уже не первый год`
